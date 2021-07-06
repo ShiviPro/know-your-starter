06c59a40c03d48f26f9505254f78dff3d6cec6ed
Shivam Tewari <www.shivamtewari@gmail.com>
2021-07-06 09:30:26 +0530

    feat: added images via pokeAPI's npm package

---
+++ assets/data/pokedata.js
@@ -0,0 +1,24 @@
+export default {
+  "result": [
+    {
+      "displayName": "Bulbasaur",
+      "id": 1
+    },
+    {
+      "displayName": "Ivysaur",
+      "id": 2
+    },
+    {
+      "displayName": "Venusaur",
+      "id": 3
+    },
+    {
+      "displayName": "Charmander",
+      "id": 4
+    },
+    {
+      "displayName": "Charmeleon",
+      "id": 5
+    }
+  ]
+}
--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,42 @@
+import * as pokeData from "../data/pokedata";
+const pokeInput = document.querySelector("#pokemon__name");
 
+const pokeOutput = document.querySelector("#poke__info");
+
+const url = "https://pokeapi.co/api/v2/pokemon";
+
+const pokeImg = document.querySelector("#pokemon__img");
+
+var pokemonDetails;
+
+function processPokeIndex(jsonOutput) {
+  pokemonDetails = jsonOutput;
+
+  console.log(pokemonDetails);
+  let pokemonIndex = pokemonDetails.id;
+
+  const imgPath =
+    "./node_modules/pokemon-sprites/sprites/pokemon/other/official-artwork/" +
+    pokemonIndex +
+    ".png";
+
+  pokeImg.setAttribute("src", imgPath);
+}
+
+function handleAPIErrors(error) {
+  pokeImg.setAttribute("alt", "Image not found");
+}
+
+const getPokeIndex = function (e) {
+  if (e.which === 13) {
+    pokeImg.setAttribute("src", "");
+    fetch(url + "/" + pokeInput.value.toLowerCase())
+      .then((output) => output.json())
+      .then(processPokeIndex)
+      .catch(handleAPIErrors);
+  }
+};
+
+console.log(pokeData);
+
+pokeInput.addEventListener("keydown", getPokeIndex);
